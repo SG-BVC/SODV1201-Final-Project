08c0fe6b54cbb66f4537baace9e171b1b3a420dd
Austyn <184693011+SG-BVC@users.noreply.github.com>
2025-03-11 18:54:55 -0600

    Create User signup + server for json data, reorder files

---
+++ js/global.js
@@ -0,0 +1,3 @@
+// For all html/js files
+const PORT = 3000
+module.exports = { PORT }; // Make it available for all js files
---
+++ js/signup.js
@@ -0,0 +1,29 @@
+const { PORT } = require("./global");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const signup_form = document.getElementById("signup_form");
+    const message = document.getElementById("message");
+
+    signup_form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent blank submissons
+
+        const user_data = {
+            name: document.getElementById("name").value,
+            phone: document.getElementById("phone").value,
+            email: document.getElementById("email").value,
+            role: document.getElementById("role").value
+            // Add Passwords for login
+        };
+
+        fetch(`http://localhost:${PORT}/signup`, { // For some reason, I need a literal path for fetch to work, need to fix for real servers
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user_data)
+        })        
+        .then(response => response.json())
+        .then(data => {
+            message.innerText = data.message;
+        })
+        .catch(error => console.error("Error:", error));
+    });
+});
